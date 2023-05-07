b3e499aebeac9d3efbdc545705a8569d75c67a1d
Fanis <mrduck597@gmail.com>
2023-05-08 01:12:47 +0500

    test 2

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -6,15 +6,13 @@ public class CoordinatePane {
     public void printQuadrant(int x, int y) {
         if (x==0 && y==0){
             System.out.println("zero");
-        } else if (y > 0) {
-            if (x>0){
-                System.out.println("first");
-            }
+        } else if (y > 0 && x>0) {
+            System.out.println("first");
+        } else if (y > 0 && x<0){
             System.out.println("second");
+        } else if (y <0 && x<0){
+            System.out.println("third");
         } else {
-            if (x<0){
-                System.out.println("third");
-            }
             System.out.println("fourth");
         }
     }
