f2e0732cf81abaa58da597429410d420ded512a9
Fanis <mrduck597@gmail.com>
2023-05-08 00:26:46 +0500

    test 1

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -1,6 +1,21 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import javax.crypto.spec.PSource;
+
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (x==0 && y==0){
+            System.out.println("zero");
+        } else if (y > 0) {
+            if (x>0){
+                System.out.println("first");
+            }
+            System.out.println("second");
+        } else {
+            if (x<0){
+                System.out.println("third");
+            }
+            System.out.println("fourth");
+        }
     }
 }
